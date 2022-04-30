44a6751977938041e0fba794bd55c55a38dd61b5
Josivan Whesley <jowhesley03@gmail.com>
2022-04-29 23:33:09 -0300

    comecando js

--- acess/js/script.js
+++ acess/js/script.js
@@ -1,1 +1,20 @@
-console.log('Oi eu sou o JavasCript')
+var people = [{
+    name: 'Josivan Whesley',
+    tel: '84999999999',
+    xp: true 
+},
+{
+    name: 'João Arhur',
+    tel: '84999999999',
+    xp: true 
+},
+{
+    name: 'Edvania Pereira',
+    tel: '84999999999',
+    xp: false 
+},
+]
+
+for (person in people){
+    console.log(people[person].name)
+}
