12fc4487540acd44faf0b5fde24099e022c6c091
Josivan Whesley <jowhesley03@gmail.com>
2022-06-15 21:37:13 -0300

    continuando

--- acess/js/script.js
+++ acess/js/script.js
@@ -20,20 +20,23 @@ function desenhaTabela(){
             xp: false,
         },
     ]
+
+    document.querySelectorAll('table.list tbody .dinamic-content')
     //person é o indice do array é apenas um número
     //people vai ser o objeto
     for (person in people) {
         // o style dá uma dinamicidade conforme a posição diferente da dinamicidade conforme o conteudo
         document.querySelector('table.list tbody').innerHTML +=
-        `<tr style="background-color: ${((person % 2 == 0) ? '#fff' : '#eee')}"> 
+        `<tr class="dinamic-content" style="background-color: ${((person % 2 == 0) ? '#fff' : '#eee')}"> 
 
         <td> ${people[person].name }</td>
         <td> ${people[person].tel }</td>
         <td> ${ (people[person].xp ? '<strong style="color:green"> Sim </strong>' : '<strong style="color:red"> Não </strong>' )}</td>
         <td>
-            <button onclick="people.splice(${person}, )"> Excluir </button>
+            <button onclick="people.splice(${person}, 1)"> Excluir </button>
         </td>
         
         </tr>`
     }
 }
+
