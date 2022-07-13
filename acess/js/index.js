ea03115616ceb85d27b424492a063aa8c4d97a67
Josivan Whesley <jowhesley03@gmail.com>
2022-07-12 22:17:44 -0300

    refazendo e dando certo

--- acess/js/index.js
+++ acess/js/index.js
@@ -1,40 +1,33 @@
-var people = [
-    {
-        name: 'Josivan Whesley',
-        tel: '84 981097572',
-        xp: true
-    },
-    {
-        name: 'João Arthur da Silva Soares',
-        tel: '84 981397432',
-        xp: true
-    },
-    {
-        name: 'Edvania Whesley',
-        tel: '84 981023223',
-        xp: false
-    },
-    {
-        name: 'Juliane Soares',
-        tel: '84 981123322',
-        xp: false
-    }
-]
-
-for (person in people){
-    document.querySelector('table.lista tbody').innerHTML +=`<tr style="background-color: ${((person % 2 == 0) ? '#fff' : '#eee')}">
-        <td>
-            ${people[person].name}        
-        </td>
-        <td>
-            ${people[person].tel}
-        </td>
-        <td>
-            ${(people[person].xp ? '<strong style="color: green"> Sim </strong>' : '<strong style="color: red"> Não </strong>')}
-        </td>
-        <td>
-        <button>Alterar</button>
-        </td>
-    </tr>` 
-
+var peopleRaw = localStorage.getItem('people')
+if (peopleRaw != null) {
+    var people = JSON.parse(peopleRaw)
+} else{
+    var people = [];
 }
+
+
+function desenhaTabela(){
+        currentLines = [...document.querySelectorAll('table.lista tbody .dinamic-content')];
+        currentLines.forEach((element) => {
+            element.remove()
+        })
+
+    for (person in people){
+        document.querySelector('table.lista tbody').innerHTML +=`<tr class="dinamic-content" style="background-color: ${((person % 2 == 0) ? '#fff' : '#eee')}">
+            <td>
+                ${people[person].name}        
+            </td>
+            <td>
+                ${people[person].tel}
+            </td>
+            <td>
+                ${(people[person].xp ? '<strong style="color: green"> Sim </strong>' : '<strong style="color: red"> Não </strong>')}
+            </td>
+            <td>
+            <button onclick="people.splice(${person},1); desenhaTabela();">Exlcuir</button>
+            </td>
+        </tr>` 
+
+    }
+}
+desenhaTabela()
